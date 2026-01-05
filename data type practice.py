3235691cbc7037ca950e77aecc2b367d9df6f43e
Taher-Trivedi <tahertrivedi11@gmail.com>
2026-01-05 11:19:20 +0530

    Add files via upload

---
+++ data type practice.py
@@ -0,0 +1,36 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="tomorrow"
+print(x)
+tomorrow
+x=45
+print(x)
+45
+x=78.90
+print(x)
+78.9
+x=4j
+print(x)
+4j
+x=["mic","speaker","phone"]
+print(x)
+['mic', 'speaker', 'phone']
+x=("mobile","laptop","p.c.")
+print(x)
+('mobile', 'laptop', 'p.c.')
+x={"name":"joy","age":45}
+print(x)
+{'name': 'joy', 'age': 45}
+x=True
+print(x)
+True
+x=?
+SyntaxError: invalid syntax
+x=none
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    x=none
+NameError: name 'none' is not defined. Did you mean: 'None'?
+x=None
+print(x)
+None
